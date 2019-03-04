bb3505c25c29b89bc3da7fc12da12a0c4b820cd5
Eric Constantinides <eric@ericconstantinides.com>
2019-03-03 22:01:40 -0800

    Update deploy (+13 squashed commits) Squashed commits: [54fe761] Update deploy [de3958f] Update deploy [b43201c] Update deploy [65c852b] update deploy [8d809ea] update deploy [cdaca99] update deploy [aa3aa6f] Update deploy [55c65ab] update deploy [1f9955f] Start with a next server [fd89ee0] Update deploy [d821933] Update deploy [78344eb] Update build port [b4d9118] Update deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ericconstantinides.com',
+      script: './server.js'
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'eric',
+      host: 'eric2',
+      ref: 'origin/master',
+      repo: 'git@github.com:ericconstantinides/ericconstantinides.com.git',
+      path: '/var/www/node/ericconstantinides.com',
+      'post-deploy':
+        'rm -Rf node_modules && npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
