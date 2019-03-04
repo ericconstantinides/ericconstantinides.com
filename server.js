6f22bf38fb315c6adabbbed9d7a6e2a32f3afab3
Eric Constantinides <eric@ericconstantinides.com>
2019-03-03 23:55:36 -0800

    remove NODE_ENV=production

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
-// const dev = false
-const app = next({ dev })
+const app = next({ dev: false })
 const handle = app.getRequestHandler()
 
 app.prepare()
