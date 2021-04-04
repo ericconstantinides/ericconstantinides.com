c6b020e5a8e1b854d3b6553882977c635af53ae3
Eric Constantinides <eric@ericconstantinides.com>
2021-04-04 16:33:52 -0600

    update pdf resume in the right place

--- utils/utils.js
+++ utils/utils.js
@@ -2,3 +2,6 @@ export const splitArray = arr => [
   arr.filter((item, index) => index + 1 <= Math.round(arr.length / 2)),
   arr.filter((item, index) => index + 1 > Math.round(arr.length / 2))
 ]
+
+export const createRandomHash = (hashSize = 6) =>
+  '0'.repeat(hashSize).replace(/0/g, () => (~~(Math.random() * 16)).toString(16))
