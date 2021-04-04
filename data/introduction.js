5a1355982da4cb339a8b631d68312b16faaed9c6
Eric Constantinides <eric@ericconstantinides.com>
2021-04-04 16:20:53 -0600

    fix typo

--- data/introduction.js
+++ data/introduction.js
@@ -1,7 +1,7 @@
 const introduction = {
   resume: `<p><strong>In a nutshell:</strong> I'm a passionate <strong>UI Architect</strong> specializing in <strong>JavaScript</strong>, <strong>React</strong>, <strong>CSS</strong>, and <strong>Vanilla JS</strong>. Most importantly, I always strive for an <strong>Amazing User Experience</strong>!</p>
     <p><strong>Most Recently:</strong> I'm a UI Architect at <a href='https://www.automationanywhere.com/' target='_blank'>Automation Anywhere</a> working on the <a href='https://www.automationanywhere.com/products/automation-360' target='_blank'>Automation 360 platform</a> using React and JavaScript.</p>
-    <p><strong>Previously:</strong> I was working on the <a href='https://developer.automationanywhere.com'>Automation Anywhere Developer Portal</a>, a React / <a href='https://nextjs.org/' target='_blank'>Next.js</a> web app ane the <a href='https://botstore.automationanywhere.com/'>Automation Anywhere Bot Store</a>. <strong>Before that</strong> I was a UI Engineer a Insureon in San Jose, Ca.`,
+    <p><strong>Previously:</strong> I was working on the <a href='https://developer.automationanywhere.com'>Automation Anywhere Developer Portal</a>, a React / <a href='https://nextjs.org/' target='_blank'>Next.js</a> web app and the <a href='https://botstore.automationanywhere.com/'>Automation Anywhere Bot Store</a>. <strong>Before that</strong> I was a UI Engineer a Insureon in San Jose, Ca.`,
   homepage: `My name is Eric Constantinides. I'm a User Interface Architect specializing in thoughtful UI/UX, React, Sass, and Vanilla JS. This is a collection of the projects that I've worked on.`
 }
 
