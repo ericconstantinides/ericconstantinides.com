25f5a725afe4dabbbcb7d3c938f72ce0bf9ee626
Eric Constantinides <eric@ericconstantinides.com>
2022-03-02 15:28:32 -0600

    upgrade modules

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       repo: 'git@github.com:ericconstantinides/ericconstantinides.com.git',
       path: '/var/www/node/ericconstantinides.com',
       'post-deploy':
-        'rm -Rf node_modules && rm -Rf .next && npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
+        'rm -Rf node_modules && rm -Rf .next && npm install --legacy-peer-deps && npm run build && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
